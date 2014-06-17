5a5919c556c279a92b8506d9c4162172342e3b39
Marko Aleksic <psybaron@gmail.com>
2014-06-17 22:58:48 +0200

    fixed error pages and site description

--- _lib/header.php
+++ _lib/header.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Carnia Design :: Amazing user experiences!</title>
-        <meta name="description" content="Carnia Design is digital agency focused on delivering amazing user experiences and creating business value through knowledge and cutting-edge innovative technology.">
+        <meta name="description" content="Carnia Design is digital agency focused on delivering amazing user experiences and creating business value through knowledge, innovation and cutting-edge technology.">
         <meta name="keywords" content="web design, web development, php, laravel, interactive agency, marketing agency, creative agency, digital agency, internet marketing, маркетинг агенција, интернет маркетинг, branding, brands, user experience, social media, e-commerce, e-business">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +16,7 @@
         <meta property="og:type" content="website">
         <meta property="og:url" content="http://carniadesign.com/">
         <meta property="og:image" content="http://carniadesign.com/img/logo.jpg">
-        <meta property="og:description" content="Carnia Design is digital agency focused on delivering amazing user experiences and creating business value through knowledge and cutting-edge innovative technology.">
+        <meta property="og:description" content="Carnia Design is digital agency focused on delivering amazing user experiences and creating business value through knowledge, innovation and cutting-edge technology.">
         <meta property="og:site_name" content="Carnia Design">
         <meta property="fb:admins" content="100000414319538">
 
