7fbefe385a18ed79dbad84f270ae735c35f18ba3
Marko Aleksic <psybaron@gmail.com>
2014-06-17 13:42:15 +0200

    fixed layouts

--- _lib/content_en.php
+++ _lib/content_en.php
@@ -1,48 +1,54 @@
 <div class="container">
-    <div class="row" id="hero">
+    <div class="row">
         <div class="col-md-12 text-center">
-            <h1 style="font-size:160px">Carnia Design</h1>
-            <p class="lead">
-                We create unique user experiences.<br>Our specialities are identity design, web site design and developement and web apps!
-            </p>
+            <section id="hero">
+                <h1>Carnia Design</h1>
+                <p class="lead">
+                    We create unique user experiences.<br>Our specialities are identity design, web site design and developement and web apps!
+                </p>
+            </section>
         </div>
     </div>
     <hr>
-    <div class="row" id="content">
+    <div class="row">
         <div class="col-md-6">
-            <h2>Services <small>what we do</small></h2>
-            <p>
-                <strong>Branding</strong>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
-            </p>
-            <p>
-                <strong>Web Design &amp; Development</strong>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
-            </p>
-            <p>
-                <strong>Web Apps</strong>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
-            </p>
-            <p>
-                <strong>Social Media</strong>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
-            </p>
-            <p>
-                <strong>Support Services</strong>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
-            </p>
+            <section id="services">
+                <h2>Services <small>what we do</small></h2>
+                <p>
+                    <strong>Branding</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
+                </p>
+                <p>
+                    <strong>Web Design &amp; Development</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
+                </p>
+                <p>
+                    <strong>Web Apps</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
+                </p>
+                <p>
+                    <strong>Social Media</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
+                </p>
+                <p>
+                    <strong>Support Services</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, inventore.
+                </p>
+            </section>
         </div>
         <div class="col-md-6">
-            <h2>Contact <small>get in touch</small></h2>
-            <p>
-                <address>
-                    <strong>Carnia Design</strong><br>
-                    Kozjacka 44<br>
-                    1300 Kumanovo, <abbr title="Macedonia">MK</abbr><br>
-                    <?php echo mungemail('info@carniadesign.com'); ?><br>
-                    +389 70 799 701
-                </address>
-            </p>
+            <section id="contact">
+                <h2>Contact <small>get in touch</small></h2>
+                <p>
+                    <address>
+                        <strong>Carnia Design</strong><br>
+                        Kozjacka 44<br>
+                        1300 Kumanovo, <abbr title="Macedonia">MK</abbr><br>
+                        <?php echo mungemail('info@carniadesign.com'); ?><br>
+                        +389 70 799 701
+                    </address>
+                </p>
+            </section>
         </div>
     </div>
     <hr>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php include '_lib/functions.php'; ?>
 <?php include '_lib/header.php'; ?>
-<section role="content">
-    <?php include "_lib/content_{$lang}.php"; ?>
-</section>
+    <section role="content">
+        <?php include "_lib/content_{$lang}.php"; ?>
+    </section>
 <?php include '_lib/footer.php'; ?>
